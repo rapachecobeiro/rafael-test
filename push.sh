b8a0fa8584e004137a9463fbf982a8f535746bd9
ribeira <rafael_ribeiro@manulife.com>
2020-02-21 09:51:05 -0500

    provisioning cli push

--- push.sh
+++ push.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# cp bin/provisioning-cli .
+
+provisioning-cli config set-token $PUSH_TOKEN
+provisioning-cli config set-api provisioning.platform.manulife.io
+provisioning-cli config initInteractiveApproval
+
 provisioning-cli app push \
     --appName "hello-actions" \
     --manifestFileName 'manifest.yml' \
     --framework "node" \
-    --routes "hello-actions.apps.cac.pcf.manulife.com"
+    --sourcePath "." \
+    --org "GSD-CAC-DEV" \
+    --space "GEES-CAC-DEV" \
+    --foundation "CAC" \
+    --routes "hello-actions.apps.cac.preview.pcf.manulife.com" \
+    --approvalApi "enterprise-pipeline-approval.platform.manulife.io" \
+    --teamEmail "Rafael_Ribeiro@manulife.com"
+
+rm package.zip
